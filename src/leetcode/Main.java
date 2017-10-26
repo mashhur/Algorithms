a713581fca446f6676d05c7d34c98e506deff4f7
mashhur <mashhur@tmon.co.kr>
2017-10-26 23:01:45 +0900

    leetcode contest

---
+++ src/leetcode/Main.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+/**
+ * Created by mashhur on 10/22/17.
+ */
+public class Main {
+}
---
+++ src/leetcode/contest/weeklycontets55/rangemodule/RangeModule.java
@@ -0,0 +1,52 @@
+package leetcode.contest.weeklycontets55.rangemodule;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Created by mashhur on 10/26/17.
+ */
+class TestRangeModule {
+
+    public static void main(String args[]) {
+        int left = 0, right = 100;
+        RangeModule obj = new RangeModule();
+        obj.addRange(left,right);
+        boolean param_2 = obj.queryRange(left,right);
+        obj.removeRange(left,right);
+    }
+}
+
+class RangeModule {
+
+    int min = Integer.MAX_VALUE;
+    int max = Integer.MIN_VALUE;
+    TreeMap<Integer, Integer> map;
+
+    Map<Integer, Boolean> hashMap;
+    public RangeModule() {
+        hashMap = new HashMap<>();
+    }
+
+    public void addRange(int left, int right) {
+        min = Math.min(min, left);
+        max = Math.max(max, right);
+    }
+
+    public boolean queryRange(int left, int right) {
+        if(min == Integer.MAX_VALUE && max == Integer.MIN_VALUE)
+            return false;
+
+        // quick search and find range start/stop positions
+        if(left < min)
+            min = left;
+
+
+        return false;
+    }
+
+    public void removeRange(int left, int right) {
+
+    }
+}
