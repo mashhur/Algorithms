d2377de055b72088acd2651a91a6f033a4c55a4b
Mashhur <mashhur.sattorov@gmail.com>
2019-01-06 05:59:34 +0900

    Reorganize project

---
+++ leetcode/MyCalendarTwo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class MyCalendarTwo {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        NumberComplement(sc.nextInt());
+        //System.out.println(findComplement(sc.nextInt()));
+    }
+}
+
