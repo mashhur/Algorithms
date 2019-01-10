c9c8d670d7c7e97d802ce9d346578c435b92ad0d
Mashhur <mashhur.sattorov@gmail.com>
2019-01-10 09:09:04 +0900

    Virtual weekly-68 tried.

---
+++ leetcode/contest/weekly68/MaxChunksSorted.java
@@ -0,0 +1,18 @@
+package leetcode.contest.weekly68;
+
+// Max Chunks To Make Sorted I
+public class MaxChunksSorted {
+
+    public int maxChunksToSorted(int[] arr) {
+        if(arr.length==0) return 0;
+
+        int sum = arr[0];
+        int chunks = 1;
+
+        for(int i=1; i<arr.length; i++) {
+            if(sum==0) chunks++;
+            sum += arr[i]-i;
+        }
+        return chunks;
+    }
+}
