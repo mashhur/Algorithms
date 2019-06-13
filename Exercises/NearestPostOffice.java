4a8a7c69f29ee1998a13169f2dcbb6397edb0ce0
Mashhur <mashhur.sattorov@gmail.com>
2019-06-13 17:17:43 +0900

    Nearest post office & security/wall problems tried.

---
+++ Exercises/NearestPostOffice.java
@@ -0,0 +1,103 @@
+package Exercises;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class NearestPostOffice {
+
+    class Point {
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    /**
+     * 0 0 P 0 0 0 0
+     * 0 0 0 0 0 P 0
+     * 0 0 P 0 0 0 0
+     * <p>
+     * 2 1 P 1 2 1 2
+     * 3 2 1 2 1 P 1
+     * 2 1 P 1 2 1 2
+     **/
+
+    private void bfs(int[][] grid) {
+        print(grid);
+
+        Queue<Point> offices = new LinkedList<>();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 'P')
+                    offices.add(new Point(i, j));
+            }
+        }
+
+        while (offices.isEmpty() == false) {
+            Point office = offices.remove();
+            bfs(office.x, office.y, grid);
+        }
+    }
+
+    void bfs(int col, int row, int[][] grid) {
+        int distance = 0;
+        int forwardCol = col + 1;
+        int backCol = col - 1;
+        int forwardRow = row + 1;
+        int backRow = row - 1;
+
+        visitNeighbours(forwardCol, row, grid, distance);
+        visitNeighbours(backCol, row, grid, distance);
+        visitNeighbours(col, backRow, grid, distance);
+        visitNeighbours(col, forwardRow, grid, distance);
+    }
+
+    private void visitNeighbours(int col, int row, int[][] grid, int distance) {
+        if (col < 0 || col >= grid.length || row < 0 || row >= grid[col].length)
+            return;
+        if (grid[col][row] == 'P')
+            return;
+        if (grid[col][row] != 0 && grid[col][row] <= distance)
+            return;
+
+        distance++;
+        grid[col][row] = distance;
+
+        print(grid);
+
+        int forwardCol = col + 1;
+        int backCol = col - 1;
+        int forwardRow = row + 1;
+        int backRow = row - 1;
+
+        visitNeighbours(forwardCol, row, grid, distance);
+        visitNeighbours(backCol, row, grid, distance);
+        visitNeighbours(col, backRow, grid, distance);
+        visitNeighbours(col, forwardRow, grid, distance);
+    }
+
+    private void print(int[][] grid) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {
+                {0, 0, 'P', 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 'P', 0},
+                {0, 0, 'P', 0, 0, 0, 0}
+        };
+
+        NearestPostOffice office = new NearestPostOffice();
+        office.bfs(grid);
+    }
+
+}
--- Exercises/SecurityWalls.java
+++ Exercises/SecurityWalls.java
@@ -1,44 +1,53 @@
 package Exercises;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
-
-class Point {
-    int x;
-    int y;
-
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
+import java.util.Queue;
 
 public class SecurityWalls {
+
+    class Point {
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    List<Point> securities;
+    List<Point> walls;
+    int[][] grid;
+
+    public void initialize() {
+        securities = new ArrayList<>();
+        securities.add(new Point(1, 1));
+        securities.add(new Point(1, 5));
+
+        walls = new ArrayList<>();
+        walls.add(new Point(2, 2));
+        walls.add(new Point(2, 5));
+
+        grid = new int[3][7];
+    }
 
     /**
      * 0 0 0 0 0 0 0
      * 0 s 0 0 0 s 0
      * 0 0 w 0 s w 0
      * <p>
-     * 2 1 2 0 0 0 0
-     * 1 s 1 2 1 s 0
-     * 2 1 w 1 s w 0
+     * 2 1 2 3 2 1 2
+     * 1 s 1 2 1 s 1
+     * 2 1 w 1 s w 1
      **/
 
     public static void main(String[] args) {
         SecurityWalls securityWalls = new SecurityWalls();
+        securityWalls.initialize();
 
-        List<Point> securities = new ArrayList<>();
-        securities.add(new Point(1, 1));
-        securities.add(new Point(1, 5));
-
-        List<Point> walls = new ArrayList<>();
-        walls.add(new Point(2, 2));
-        walls.add(new Point(2, 5));
-
-        int[][] grid = new int[3][7];
-
-        securityWalls.calcShortestPath(grid, securities, walls);
+        securityWalls.calcShortestPath();
     }
 
     private void print(int[][] grid) {
@@ -48,35 +57,61 @@ public class SecurityWalls {
             }
             System.out.println();
         }
+        System.out.println();
     }
 
-    public void calcShortestPath(int[][] grid, List<Point> securities, List<Point> walls) {
-        print(grid);
+    public void calcShortestPath() {
+        Queue<Point> queue = new LinkedList<>();
+
         for (Point point : securities) {
             grid[point.x][point.y] = 's';
-            checkForNeighbours(grid, point.x++, point.y);
-            checkForNeighbours(grid, point.x--, point.y);
-            checkForNeighbours(grid, point.x, point.y--);
-            checkForNeighbours(grid, point.x, point.y++);
+            queue.add(point);
         }
-
         for (Point point : walls) {
             grid[point.x][point.y] = 'w';
-
-            checkForNeighbours(grid, point.x++, point.y);
-            checkForNeighbours(grid, point.x--, point.y);
-            checkForNeighbours(grid, point.x, point.y--);
-            checkForNeighbours(grid, point.x, point.y++);
+            queue.add(point);
         }
+
+        print(grid);
+
+        // BFS
+        while (queue.isEmpty() == false) {
+            Point point = queue.remove();
+
+            int distance = 0;
+            int backCol = point.x - 1;
+            int forwardCol = point.x + 1;
+            int forwardRow = point.y - 1;
+            int backRow = point.y + 1;
+            checkForNeighbours(grid, backCol, point.y, distance);
+            checkForNeighbours(grid, forwardCol, point.y, distance);
+            checkForNeighbours(grid, point.x, backRow, distance);
+            checkForNeighbours(grid, point.x, forwardRow, distance);
+        }
+
         System.out.println(" --- Result --- ");
         print(grid);
     }
 
-    private void checkForNeighbours(int[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length ||
-                grid[i][j] == 's' || grid[i][j] == 'w')
+    private void checkForNeighbours(int[][] grid, int i, int j, int distance) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) // grid boundary validation
+            return;
+        if (grid[i][j] == 's' || grid[i][j] == 'w') // security or wall
+            return;
+        if (grid[i][j] != 0 && grid[i][j] <= distance) // already visited and calculated distance
             return;
 
-        grid[i][j] = 1;
+        distance++;
+        grid[i][j] = distance;
+        print(grid);
+
+        int backCol = i - 1;
+        int forwardCol = i + 1;
+        int forwardRow = j - 1;
+        int backRow = j + 1;
+        checkForNeighbours(grid, backCol, j, distance);
+        checkForNeighbours(grid, forwardCol, j, distance);
+        checkForNeighbours(grid, i, backRow, distance);
+        checkForNeighbours(grid, i, forwardRow, distance);
     }
 }
